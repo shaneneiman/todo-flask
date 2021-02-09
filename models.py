61289165ed2b9e054b2e1a705e8c7b2a022db44d
shaneneiman <shane.neiman@gmail.com>
2021-02-09 01:12:13 -0500

    update env variable

--- models.py
+++ models.py
@@ -3,12 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_user import UserMixin
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class User(db.Model, UserMixin):
     __tablename__ = "users"
+
     id = db.Column(db.Integer, primary_key=True)
     active = db.Column("is_active", db.Boolean(), nullable=False, server_default="1")
     username = db.Column(db.String(100, collation="NOCASE"), nullable=False, unique=True)
@@ -20,6 +21,7 @@ class User(db.Model, UserMixin):
 
 class Todo(db.Model):
     __tablename__ = "todos"
+    
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(250), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, server_default=False)
