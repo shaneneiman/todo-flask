e24f64e385ddbfd8614758aa702d1de7d21b26a8
shaneneiman <shane.neiman@gmail.com>
2021-02-12 09:36:35 -0500

    update engine options

--- app.py
+++ app.py
@@ -15,9 +15,6 @@ app.config['DEBUG'] = os.environ.get("DEBUG")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
-    'pool': QueuePool._creator,
-    'pool_size': 10,
-    'pool_recycle': 120,
     'pool_pre_ping': True
 }
 app.config['DEBUG'] = os.environ.get("DEBUG")
